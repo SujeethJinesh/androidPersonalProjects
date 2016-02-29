536ee01f050303257c1a0ed6b890ee657c4ef942
Sujeeth Jinesh <sujeethjinesh@gmail.com>
2016-02-28 23:11:02 -0500

    deleted unnecessary files

--- androidPersonalProjects/Nutri-Potum3.0/app/src/main/java/app/sunshine/android/example/com/nutri_potum30/FlavoringActivity.java
+++
@@ -1,9 +0,0 @@
-package app.sunshine.android.example.com.nutri_potum30;
-
-import android.support.v4.app.Fragment;
-
-/**
- * Created by lab on 1/11/2016.
- */
-public class FlavoringActivity extends Fragment {
-}
--- androidPersonalProjects/Nutri-Potum3.0/app/src/main/java/app/sunshine/android/example/com/nutri_potum30/SettingsActivity.java
+++
@@ -1,7 +0,0 @@
-package app.sunshine.android.example.com.nutri_potum30;
-
-/**
- * Created by lab on 1/10/2016.
- */
-public class SettingsActivity {
-}
